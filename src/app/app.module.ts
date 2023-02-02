20fc232f45c04904965ebd27f18d2b92eb9bf583
Diego Queiroz <diego.queiroz@economia.gov.br>
2023-02-02 20:26:41 -0300

    Timeline adicionada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { MzdTimelineModule } from 'ngx-mzd-timeline';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCommonModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { FormsModule } from '@angular/forms';
+
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faAsterisk, faBan, faBell as faSolidBell, faBriefcase, faCamera, faCircle, faCloud, faCog,
+  faEnvelopeOpen, faHandPointLeft, faMobile, faMoon, faPlay, faSkating, faSkiing, faSkiingNordic,
+  faSmileBeam, faSmileWink, faSnowboarding, faSpinner,
+  faSquare, faStar, faSun, faSwimmer, faSync
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MzdTimelineModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCommonModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faSmileWink, faSmileBeam, faEnvelopeOpen, faCloud,
+      faMobile, faSquare, faSpinner, faCircle,
+      faSync, faPlay, faSun, faMoon, faStar,
+      faHandPointLeft, faAsterisk, faCog, faSkating,
+      faSkiing, faSkiingNordic, faSnowboarding, faSwimmer,
+      faSolidBell, faCamera, faBan, faBriefcase);
+  }
+}
